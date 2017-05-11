9643bab212544bfc34add59b9dca4fe93f9325e3
WMJ <w.mingjian@yahoo.com>
2017-05-11 16:40:56 +0800

    重构数据表

--- Server/Resource/admin.py
+++ Server/Resource/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import School, User, Group, Group_User
+from .models import School, User, Group, Group_User, Affairs, SingleAffairsTime, SuccessiveAffairsTime, HaveAffair, InGroup
 
 # Register your models here.
-admin.site.register((School, User, Group, Group_User))
+admin.site.register((School, User, Group, Group_User, Affairs, SingleAffairsTime, SuccessiveAffairsTime, HaveAffair, InGroup))
--- Server/Resource/models.py
+++ Server/Resource/models.py
@@ -1,32 +1,25 @@
 from django.db import models
 from django.contrib.auth.hashers import make_password, check_password
 
-# Create your models here.
-
 class School(models.Model):
-    school_id = models.CharField(max_length=10, primary_key=True)
-    school_name = models.CharField(max_length=30)
-    school_province = models.CharField(max_length=20)
-    school_city = models.CharField(max_length=30)
+    id = models.CharField(max_length=10, primary_key=True)
+    name = models.CharField(max_length=30)
+    province = models.CharField(max_length=20)
+    city = models.CharField(max_length=30)
     def __str__(self):
-        return self.school_name
+        return self.name
 
 class User(models.Model):
     user_id = models.CharField(max_length=20, primary_key=True)
     password = models.CharField(max_length=16)
-    stu_id = models.CharField(max_length=50)
+    stu_id = models.CharField(max_length=50)  # 学号
     nickname = models.CharField(max_length=15)
-    realname = models.CharField(max_length=10)
-    gender = models.CharField(max_length=2,
-                            choices=(
-                                ('男', '男'),
-                                ('女', '女')
-                                )
-                            )
-    birthday = models.DateField()
-    mobile = models.IntegerField()
-    email = models.EmailField()
-    user_updatetime = models.DateField(auto_now_add=True)
+    realname = models.CharField(max_length=10, null=True)
+    gender = models.SmallIntegerField()  # 1是男, 2是女, 3以上是其他
+    birthday = models.DateField(null=True)
+    mobile = models.CharField(max_length=21)  # 手机号是21位
+    email = models.EmailField(null=True)
+    updatetime = models.DateField(auto_now_add=True)
 
     school_id = models.ForeignKey(School, on_delete=models.CASCADE)
     major = models.CharField(max_length=30)
@@ -48,16 +41,43 @@ class Group(models.Model):
 class Group_User(models.Model):
     group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    privilege = models.CharField(max_length=20, choices=(
-                                ('owner', 'owner'),
-                                ('master', 'master'),
-                                ('member', 'member'),
-                                ))
+    privilege = models.SmallIntegerField()  # 1是创建者, 2是管理员, 3是普通成员
     visible = models.BooleanField(default=True)
     update_time = models.DateTimeField(auto_now_add=True)
 
-class ScheTime(models.Model):
-    place = models.CharField(max_length=20)
-    day = models.CharField(max_length=10)
-    start_date = models.DateField()
-    end_date = models.DateField()
+
+class Affairs(models.Model):
+    id = models.CharField(max_length=10, primary_key=True)
+    name = models.CharField(max_length=20)
+    type = models.SmallIntegerField()  # 1是课程, 2是目标, 3是活动
+    place = models.CharField(max_length=30, default="未填写")
+    details = models.CharField(max_length=100, default="无")
+    organizer = models.ForeignKey(User)
+    def __str__(self):
+        return self.name
+
+
+class SingleAffairsTime(models.Model):
+    ''' 单一时间 '''
+    affair_id = models.ForeignKey(Affairs)
+    dateTime = models.DateTimeField()
+
+
+class SuccessiveAffairsTime(models.Model):
+    ''' 连续时间 '''
+    affair_id = models.ForeignKey(Affairs)
+    time = models.TimeField()
+    startData = models.DateField()
+    lastData = models.DateField()
+    every = models.SmallIntegerField()  # 从最低一位到第7位分别对于星期一到日
+
+
+class HaveAffair(models.Model):
+    user_id = models.ForeignKey(User)
+    affair_id = models.ForeignKey(Affairs)
+
+
+class InGroup(models.Model):
+    user_id = models.ForeignKey(User)
+    group_id = models.ForeignKey(Group)
+    role = models.ForeignKey(Group_User)
